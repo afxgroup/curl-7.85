e2f463949e1473150974041732ed7e3f32c35644
Andrea Palmatè <info@papersoft.it>
2025-05-02 09:12:52 +0200

    First import

---
+++ tests/configurehelp.pm
@@ -0,0 +1,23 @@
+# This is a generated file.  Do not edit.
+
+package configurehelp;
+
+use strict;
+use warnings;
+use Exporter;
+
+use vars qw(
+    @ISA
+    @EXPORT_OK
+    $Cpreprocessor
+    );
+
+@ISA = qw(Exporter);
+
+@EXPORT_OK = qw(
+    $Cpreprocessor
+    );
+
+$Cpreprocessor = 'ppc-amigaos-gcc -E -mcrt=clib4 -D__USE_INLINE__';
+
+1;
